fd3ab805f5e064b9f0c28c641990efacdc214e41
Kumar Sunderam <68020132+sandy1312@users.noreply.github.com>
2020-09-27 13:57:37 +0530

    Update Drink Water Notification.py

--- Drink Water Notification.py
+++ Drink Water Notification.py
@@ -1,11 +1,11 @@
 import time
 from plyer import notification #For desktop notification we use plyer import function.
 if __name__ == '__main__':
-    # while True:
+    while True:
         notification.notify(
             title = "**Please drink water!!",
-            message = "The National Academies of Sciences, Engineering, and Medicine determined that an adequate daily fluid intake is: About 15.5 cups (3.7 liters) of fluids for men",
+            message = "The National Academies of Sciences, Engineering, and Medicine determined that an adequate daily fluid intake is: About 15.5 cups (3.7 liters) of fluids for men.About 11.5 cups (2.7 liters) of fluids a day for women",
             timeout = 10, #For 10 second it will appear on screen
             app_icon = "E:\Python\Glass-Water.ico" #Drinking icon image
         )
-        # time.sleep(60*60) #After every 3600 second it will remind
+        time.sleep(60*60) #After every 3600 second it will remind
